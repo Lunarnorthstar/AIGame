28590c1a38cdbf8751a8dd03234f5fd986901a47
D19127919 <77803253+D19127919@users.noreply.github.com>
2022-04-19 09:02:38 +0100

    Player movement and clue collection
    Player moves with WASD, sprints with SHIFT (with stamina).
    Objects can spawn, be collected, and spawn the final object once enough of them are collected.

---
+++ AIGameDesign/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("AI variables")]
+    public static Vector3 playerPos;
+    
+    [Header("Stamina Variables")]
+    public float maxStamina = 5;
+    private float stamina;
+    public float runSpeedMult = 2;
+    public GameObject staminaBar;
+    
+    [Header("Movement variables")]
+    private Vector3 moveDirection; //The direction the player is moving in
+    private Rigidbody myRB; //The player's rigidbody
+    public Transform cameraobject;
+    public float movespeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        
+        stamina = maxStamina;
+
+        //Getting player position
+        StartCoroutine(TrackPlayer());
+    }
+    //Tracking player position
+    IEnumerator TrackPlayer()
+    {
+        while (true)
+        {
+            playerPos = gameObject.transform.position;
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        moveDirection = cameraobject.forward * Input.GetAxis("Vertical");
+        moveDirection += cameraobject.right * Input.GetAxis("Horizontal"); //These two lines get the horizontal and vertical components of movement based on player input
+        moveDirection.Normalize(); //Normalize it so it's between 0 and 1
+        moveDirection.y = 0; //Make sure you aren't going up any
+        
+        Vector3 movementVelocity = moveDirection * movespeed; //Multiply by speed to get velocity
+        
+
+        if (Input.GetKey(KeyCode.LeftShift) && stamina > 0 && (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0))//If you're sprinting and not stationary and have stamina...
+        {
+            movementVelocity *= runSpeedMult; //Multiply your speed by the relevant multiplier.
+            
+            stamina -= Time.deltaTime; //Reduce your stamina.
+        }
+
+        myRB.velocity = movementVelocity; //Apply that to your rigidbody
+
+        
+        if (!Input.GetKey(KeyCode.LeftShift) && stamina < maxStamina) //If you're not trying to sprint and your stamina is below maximum...
+        {
+            stamina += Time.deltaTime; //Gain stamina based on time passed.
+            
+        }
+
+        staminaBar.transform.localScale = new Vector3( 1000 * (stamina / maxStamina), staminaBar.transform.localScale.y, staminaBar.transform.localScale.z); //Mess with the stamina bar gameobject to shrink it based on missing stamina
+        
+    }
+}
---
+++ AIGameDesign/Assets/Scripts/PlayerPickup.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//This script goes on the player object. It handles the collection, spawning, and loss of objects on contact.
+//Dependencies; Player: a trigger collider to make contact with the target objects. Target Objects: A non-trigger collider to activate the trigger
+public class PlayerPickup : MonoBehaviour
+{
+    [Tooltip("The amount of objects the player has - no touching!")]
+    public static int objects = 0;
+    [Tooltip("The amount of objects the player needs to have before triggering the final one")]
+    public int totalObjects = 9;
+
+    [Tooltip("The UI element that displays the object count")]
+    public Text uiText; //The UI element that displays the player's object count.
+
+    public GameObject[] objectsToActivate;
+    public GameObject finalObject; //The final, special object
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < totalObjects; i++) //Run the objectPicker function an amount of times equal to the max objects
+        {
+            objectPicker();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiText.text = "Objects:" + objects; //Display current object count on UI
+    }
+
+    private void OnTriggerEnter(Collider other) //When the player bumps into something...
+    {
+        if (other.tag == "Object") //If it's an object to be picked up...
+        {
+            objects++; //Increment the object count
+            other.gameObject.SetActive(false); //Remove the object from the scene.
+        }
+
+        if (objects == totalObjects) //If you've collected all the objects...
+        {
+            finalObject.SetActive(true); //Activate the special, final one.
+        }
+    }
+
+    public void objectPicker() //Handles the spawning and respawning of objects.
+    {
+        while (true)
+        {
+            int random = UnityEngine.Random.Range(0, objectsToActivate.Length); //Get a random object in the array...
+            if (objectsToActivate[random].activeSelf == false) //If it's not already active...
+            {
+                objectsToActivate[random].SetActive(true); //Activate it.
+                break; //Stop infinite looping.
+            }
+        }
+    }
+    
+    public void LoseObject() //Handles the loss of objects. Called externally. Not necessary.
+    {
+        if (objects > 0) //If you have any objects...
+        {
+            objects--; //Lose one...
+            objectPicker(); //And spawn a random one in the scene.
+        }
+    }
+}
